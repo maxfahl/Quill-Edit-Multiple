e1a4baff7af3b17eadfa4b1fd1a3711fb11f5ae1
maxfahl <max@pixilab.se>
2020-10-07 10:10:21 +0200

    Fixed som styling differences.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
 import "@atlaskit/css-reset";
 import "quill/dist/quill.snow.css";
+import "./index.css"
+
+import React from "react";
+import ReactDOM from "react-dom";
 
 import App from "./App";
 
