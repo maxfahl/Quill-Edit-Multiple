3fffb2abfeba0cf0b9f8e54ed406d3529dc9d8fe
maxfahl <max@pixilab.se>
2020-10-15 11:11:01 +0200

    .nvmrc

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ const App = () => {
     const [activeEditable, setActiveEditable] = useState();
 
     /**
-     * Instansiate the quill editor, using the quillEditorContainer as
+     * Instantiate the quill editor, using the quillEditorContainer as
      * the element for it. Also, use our own toolbar.
      */
     useEffect(() => {
@@ -130,34 +130,31 @@ const App = () => {
                         <button className="ql-underline"></button>
                         <button className="ql-strike"></button>
                     </span>
-                    <span className="ql-formats">
-                        <select className="ql-color"></select>
-                        <select className="ql-background"></select>
-                    </span>
-                    <span className="ql-formats">
-                        <button className="ql-script" value="sub"></button>
-                        <button className="ql-script" value="super"></button>
-                    </span>
-                    <span className="ql-formats">
-                        <button className="ql-blockquote"></button>
-                        <button className="ql-code-block"></button>
-                    </span>
+                    {/*<span className="ql-formats">*/}
+                    {/*    <select className="ql-color"></select>*/}
+                    {/*    <select className="ql-background"></select>*/}
+                    {/*</span>*/}
+                    {/*<span className="ql-formats">*/}
+                    {/*    <button className="ql-script" value="sub"></button>*/}
+                    {/*    <button className="ql-script" value="super"></button>*/}
+                    {/*</span>*/}
+                    {/*<span className="ql-formats">*/}
+                    {/*    <button className="ql-blockquote"></button>*/}
+                    {/*    <button className="ql-code-block"></button>*/}
+                    {/*</span>*/}
                     <span className="ql-formats">
                         <button className="ql-list" value="ordered"></button>
                         <button className="ql-list" value="bullet"></button>
                         <button className="ql-indent" value="-1"></button>
                         <button className="ql-indent" value="+1"></button>
-                    </span>
-                    <span className="ql-formats">
-                        <button className="ql-direction" value="rtl"></button>
                         <select className="ql-align"></select>
                     </span>
-                    <span className="ql-formats">
-                        <button className="ql-link"></button>
-                        <button className="ql-image"></button>
-                        <button className="ql-video"></button>
-                        <button className="ql-formula"></button>
-                    </span>
+                    {/*<span className="ql-formats">*/}
+                    {/*    <button className="ql-link"></button>*/}
+                    {/*    <button className="ql-image"></button>*/}
+                    {/*    <button className="ql-video"></button>*/}
+                    {/*    <button className="ql-formula"></button>*/}
+                    {/*</span>*/}
                     <span className="ql-formats">
                         <button className="ql-clean"></button>
                     </span>
--- src/data/editables-initial-data.js
+++ src/data/editables-initial-data.js
@@ -26,28 +26,32 @@ export default {
 			</p>
 		`
 	},
+	// "editable-2": {
+	// 	id: "editable-2",
+	// 	content: `
+	// 		<h1>Editable 2 Header</h1>
+	// 		<p><br></p>
+	// 		<p>
+	// 		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+	// 		ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+	// 		ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+	// 		nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+	// 		est laborum.
+	// 		</p>
+	// 		<p><br></p>
+	// 		<h2>Editable 2 Sub Header</h2>
+	// 		<p><br></p>
+	// 		<p>
+	// 		Sed ut perspiciatis unde omnis iste natus error sit
+	// 		voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et
+	// 		quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit
+	// 		aut fugit, sed quia consequuntur magni.
+	// 		</p>
+	// 	`
+	// },
 	"editable-2": {
 		id: "editable-2",
-		content: `
-			<h1>Editable 2 Header</h1>
-			<p><br></p>
-			<p>
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-			ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-			ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-			nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-			est laborum.
-			</p>
-			<p><br></p>
-			<h2>Editable 2 Sub Header</h2>
-			<p><br></p>
-			<p>
-			Sed ut perspiciatis unde omnis iste natus error sit
-			voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et
-			quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit
-			aut fugit, sed quia consequuntur magni.
-			</p>
-		`
+		content: `<div id="taTextElement2838907865789850" contenteditable="true" ta-bind="ta-bind" ng-model="html" class="ng-pristine ng-untouched ng-valid ta-bind ng-not-empty" aria-invalid="false" style=""><h1><i>H1</i></h1><p>Paragraph baby</p><p><br></p><h2>​<i><b>H2</b></i></h2><p>Another <b>paragraph </b><i><b>baby</b></i></p><p><br></p><h3><b><i><u>H3</u></i></b></h3><p>A third <b>paragraph </b><i><u><b>baaby</b></u></i></p><p><i><u><br></u></i></p><ul><li><i>List</i></li><li><b>Items</b></li></ul><div><b><br></b></div><div><ol><li>One item</li><li><u>Two items</u></li></ol></div><div><b><br></b></div><div><b><br></b></div></div>`
 	},
 	"editable-3": {
 		id: "editable-3",
